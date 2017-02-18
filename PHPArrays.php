bab24ec38f96ecd55b8942d00ccf655f005c1c8d
Chris Williams <chrwilliams@csumb.edu>
2017-02-18 06:32:00 +0000

    $cards array now holds all cards containing value,suit, and path to background image

--- PHPArrays.php
+++ PHPArrays.php
@@ -10,23 +10,40 @@
     7) There is an external CSS file with 10 rules (10pts)
   */
   
-  $cards = []; // should hold 52 card objects
-  $card = [];  // holds value, suit, and background image
+  $cards = []; // should hold 52 card "objects"
+  //$card = [];  // holds value, suit, and background image
   $i;
-  $temp;
+  $j;
+  $cardInd = 0;
 
-    for ($i=0; $i<52;++$i) {
-        //echo ($i%13) . '<br />';
-        $card["value"] = (($i/13) + 1);
-        $card["Suit"] = "clubs";
-        //$card["bgImage"] = 
-       /*
-       if($condition == "parameter") {
-           $value .= "<img src='path/to/img' alt='img' />";
-       }
-
-       unset($value);
-       */
+    for ($j=1; $j<=4;++$j) {
+        for ($i = 1; $i <= 13; ++$i) {
+            $card = [];
+            $card["value"] = $i;
+            switch ($j) {
+                case 1:
+                    $card["suit"] = "clubs";
+                    $card["bgImage"] = "/Labs/Lab3/cards/clubs/".$i.".png";
+                    break;
+                case 2:
+                    $card["suit"] = "diamonds";
+                    $card["bgImage"] = "/Labs/Lab3/cards/diamonds".$i.".png";
+                    break;
+                case 3:
+                    $card["suit"] = "hearts";
+                    $card["bgImage"] = "/Labs/Lab3/cards/hearts/".$i.".png";
+                    break;
+                case 4:
+                    $card["suit"] = "spades";
+                    $card["bgImage"] = "/Labs/Lab3/cards/spades/".$i.".png";
+                    break;
+                default:
+                    break;
+            }
+                $cards[$cardInd] = $card;
+                echo $cards[$cardInd]["value"]." ". $cards[$cardInd]["suit"]." ".$cards[$cardInd]["bgImage"].'<br>';
+                $cardInd += 1;
+        }
     }
   
 ?>
