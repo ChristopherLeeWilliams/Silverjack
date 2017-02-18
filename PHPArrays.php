89bf0d6d302ee2a8dd7086c32fbe02b8f1a686d0
Chris Williams <chrwilliams@csumb.edu>
2017-02-18 07:31:16 +0000

    added printArray Function to print out all the cards + updated CSS

--- PHPArrays.php
+++ PHPArrays.php
@@ -10,8 +10,14 @@
     7) There is an external CSS file with 10 rules (10pts)
   */
   
-  $cards = []; // should hold 52 card "objects"
-  //$card = [];  // holds value, suit, and background image
+  $cardsArr = []; // should hold 52 card "objects"
+  initializeCards($cardsArr);
+
+  
+  // Test:
+  printArray($cardsArr);
+
+ function initializeCards(&$cards) {
   $i;
   $j;
   $cardInd = 0;
@@ -27,7 +33,7 @@
                     break;
                 case 2:
                     $card["suit"] = "diamonds";
-                    $card["bgImage"] = "/Labs/Lab3/cards/diamonds".$i.".png";
+                    $card["bgImage"] = "/Labs/Lab3/cards/diamonds/".$i.".png";
                     break;
                 case 3:
                     $card["suit"] = "hearts";
@@ -41,10 +47,39 @@
                     break;
             }
                 $cards[$cardInd] = $card;
-                echo $cards[$cardInd]["value"]." ". $cards[$cardInd]["suit"]." ".$cards[$cardInd]["bgImage"].'<br>';
+                //echo $cards[$cardInd]["value"]." ". $cards[$cardInd]["suit"]." ".$cards[$cardInd]["bgImage"].'<br>';
                 $cardInd += 1;
         }
     }
+ }
+ 
+ function printArray($cards) {
+        $tmp;
+        echo '<div class = "suit">';
+        for($tmp = 0; $tmp<13;$tmp++) {
+            echo '<div class = "card" style="background-image: url('. $cards[$tmp]["bgImage"].')";> </div>';
+        }
+        echo '</div>';
+        
+        
+        echo '<div class = "suit">';
+        for($tmp = 13; $tmp<26;$tmp++) {
+            echo '<div class = "card" style="background-image: url('. $cards[$tmp]["bgImage"].')";> </div>';
+        }
+        echo '</div>';
+        
+        echo '<div class = "suit">';
+        for($tmp = 26; $tmp<39;$tmp++) {
+            echo '<div class = "card" style="background-image: url('. $cards[$tmp]["bgImage"].')";> </div>';
+        }
+        echo '</div>';
+        
+        echo '<div class = "suit">';
+        for($tmp = 39; $tmp<52;$tmp++) {
+            echo '<div class = "card" style="background-image: url('. $cards[$tmp]["bgImage"].')";> </div>';
+        }
+        echo '</div>';
+ }
   
 ?>
 
