0d7df65351f91b2a73e047951319eaf205daed69
Chris Williams <chrwilliams@csumb.edu>
2017-02-19 07:39:17 +0000

    added printArray Function to print out all the cards + updated CSS

--- PHPArrays.php
+++ PHPArrays.php
@@ -1,15 +1,6 @@
 
 <?php
-  /*
-    1) Each player gets the "right amount" of cards to get close to 42 (20pts)
-    2) The cards are not duplicated (15pts)
-    3) The total points per player is displayed properly (15pts)
-    4) The winner(s) is(are) displayed properly with the earned points (15pts)
-    5) Players pictures are displayed RANDOMLY (10pts)
-    6) Your contribution in GitHub is similar to your teammates (15pts)
-    7) There is an external CSS file with 10 rules (10pts)
-  */
-  
+
   $cardsArr = []; // should hold 52 card "objects"
   initializeCards($cardsArr);
 
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Silverjack
 internet Programming Lab 3
+
+    1) Each player gets the "right amount" of cards to get close to 42 (20pts)
+    2) The cards are not duplicated (15pts)
+    3) The total points per player is displayed properly (15pts)
+    4) The winner(s) is(are) displayed properly with the earned points (15pts)
+    5) Players pictures are displayed RANDOMLY (10pts)
+    6) Your contribution in GitHub is similar to your teammates (15pts)
+    7) There is an external CSS file with 10 rules (10pts)
